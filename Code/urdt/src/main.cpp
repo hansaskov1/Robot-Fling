de351b1d29fe484b4826f60d7a088f764598c6ac
Hans Askov <haask19@student.d>
2020-09-10 14:32:11 +0200

    Change to local ip

--- Code/urdt/src/main.cpp
+++ Code/urdt/src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <ur_rtde/rtde_control_interface.h>
+#include <ur_rtde/rtde.h>
 #include <ur_rtde/rtde_receive_interface.h>
 
 
@@ -14,7 +15,7 @@ int main()
     // The constructor simply takes the IP address of the Robot
     ur_rtde::RTDEControlInterface rtde_control("127.0.0.1");
     // First argument is the pose 6d vector followed by speed and acceleration
-   // rtde_control.moveL({-90, -90, 0, -90, 0, 0}, 0.5, 0.2);
+    rtde_control.moveL({-1.31305, -1.68469, -0.8460, -0.15287, 1.592, 4.8692}, 0.2, 0.5);
 
 
     ur_rtde::RTDEReceiveInterface rtde_receive("127.0.0.1");
