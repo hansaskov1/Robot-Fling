906f51a7caac1d15b47d7aee3bc5d663f766d8a7
Hans Askov <haask19@student.d>
2020-09-10 12:59:33 +0200

    Added ur rtde files and cpp

---
+++ Code/urdt/build/CMakeFiles/ur_rtde_cmake_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ur_rtde_cmake_example.dir/main.cpp.o"
+  "ur_rtde_cmake_example.pdb"
+  "ur_rtde_cmake_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ur_rtde_cmake_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
