0940fe87b7b08701cbf9a2c1c1bdffcc060ecbe7
Hans Askov <haask19@student.d>
2020-09-10 13:12:39 +0200

    Update. programs print "hej"

--- Code/urdt/src/main.cpp
+++ Code/urdt/src/main.cpp
@@ -6,11 +6,13 @@ int main(int argc, char** argv)
 {
 
 
+    sdt::cout << "123" << std::endl;
+/*
     // The constructor simply takes the IP address of the Robot
     RTDEControlInterface rtde_control("127.0.0.1");
     // First argument is the pose 6d vector followed by speed and acceleration
     rtde_control.moveL({-0.143, -0.435, 0.20, -0.001, 3.12, 0.04}, 0.5, 0.2);
-
+*/
 
 
     return 0;
