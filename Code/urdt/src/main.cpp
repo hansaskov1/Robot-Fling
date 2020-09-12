b614dc28a000887893acc077e275fa609e1d8464
Hans Askov <haask19@student.d>
2020-09-13 00:32:06 +0200

    Added ur_rtde example and made  suso code to a potential throw method.

--- Code/urdt/src/main.cpp
+++ Code/urdt/src/main.cpp
@@ -3,32 +3,60 @@
 #include <ur_rtde/rtde_control_interface.h>
 #include <ur_rtde/rtde.h>
 #include <ur_rtde/rtde_receive_interface.h>
+#include <vector>
+#include <chrono>
+#include <thread>
 
 
+// let go of ball at throwingPose, with appropriate velocity and angle for the ball to hit target pose
+void throwBall(const std::array<double, 3> targetCoordinates, const std::array<double, 3> throwingCoordinates, const double angle){
+
+        //calculate velocity from angle and diffrence in position to hit target
+        //calculate ramp pose:
+            // 1. which is parralel with the target and throwing coordinates in the xy plane                            (calculate xy plane from target/throwing coordinates)
+            // 2. which angle between it and throwing coordinates is equal to the variable "angle" in the z coordinate  (calculate line in xy plane with parameter angle that passes through throwing coordinate)
+            // 3. which orientation is purpendicular to the line in the xy plane so the ball can
+        //calculate acceleration
+        //define ramp up pose
+        //define throwing pose
+        //calculate time at which the tcp point is at throwingCoordinate from a linear motion in carteesian coordinates and a constant acceleration.
+        //begin movement to throwing pose
+        //call gripper to open after defined time.
+
+
+
+
+
+}
+
+
+
+
+using namespace ur_rtde;
+using namespace std::chrono;
 
 int main()
 {
+    // example from ur_rtde
+    RTDEControlInterface rtde_control("127.0.0.1");
 
-    //std::cout << "hej" << std::endl;
+      double velocity = 0.5;
+      double acceleration = 0.5;
+      double blend_1 = 0.0;
+      double blend_2 = 0.02;
+      double blend_3 = 0.0;
+      std::vector<double> path_pose1 = {-0.143, -0.435, 0.20, -0.001, 3.12, 0.04, velocity, acceleration, blend_1};
+      std::vector<double> path_pose2 = {-0.143, -0.51, 0.21, -0.001, 3.12, 0.04, velocity, acceleration, blend_2};
+      std::vector<double> path_pose3 = {-0.32, -0.61, 0.31, -0.001, 3.12, 0.04, velocity, acceleration, blend_3};
 
+      std::vector<std::vector<double>> path;
+      path.push_back(path_pose1);
+      path.push_back(path_pose2);
+      path.push_back(path_pose3);
 
-    // The constructor simply takes the IP address of the Robot
-    ur_rtde::RTDEControlInterface rtde_control("127.0.0.1");
-    // First argument is the pose 6d vector followed by speed and acceleration
-    rtde_control.moveL({-1.31305, -1.68469, -0.8460, -0.15287, 1.592, 4.8692}, 0.2, 0.5);
+      // Send a linear path with blending in between - (currently uses separate script)
+      rtde_control.moveL(path);
+      rtde_control.stopScript();
 
-
-    ur_rtde::RTDEReceiveInterface rtde_receive("127.0.0.1");
-    std::vector<double> joint_positions = rtde_receive.getActualQ();
-
-
-    for (double position: joint_positions){
-        std::cout << position << std::endl;
-    }
-
-
-
-
-
-    return 0;
+      return 0;
 }
