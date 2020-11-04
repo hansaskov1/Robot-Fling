1a1964e36d61b9d0343e0111c475cd83b64d8fe8
Hans Askov <haask19@student.d>
2020-11-04 12:38:17 +0100

    Cleaned up path.h

--- SemesterProjekt/path.h
+++ SemesterProjekt/path.h
@@ -6,47 +6,33 @@
 class Path
 {
 public:
-    Path(){
+    Path()
+    {
         jointPoses.reserve(100);
         jointVel.reserve(100);
         toolPose.reserve(100);
         toolVel.reserve(100);
     }
 
-    void addPoint (const std::vector<double> &jointPosesValue, const std::vector<double> &jointVelValue, const std::vector<double> &toolPoseValue, const std::vector<double> &toolVelValue) {
+    void addPoint (const std::vector<double> &jointPosesValue, const std::vector<double> &jointVelValue, const std::vector<double> &toolPoseValue, const std::vector<double> &toolVelValue)
+    {
         jointPoses.push_back(jointPosesValue);
         jointVel.push_back(jointVelValue);
         toolPose.push_back(toolPoseValue);
         toolVel.push_back(toolVelValue);
     }
 
-    std::vector<std::vector<double> > getJointPoses() const {
-        return jointPoses;
-    }
-    void addJointPose(const std::vector<double> &value){
-        jointPoses.push_back(value);
-    }
+    std::vector<std::vector<double> > getJointPoses() const {return jointPoses;}
+    void addJointPose(const std::vector<double> &value)     {jointPoses.push_back(value);}
 
-    std::vector<std::vector<double> > getJointVel() const {
-        return jointVel;
-    }
-    void addJointVel(const std::vector<double> &value){
-        jointVel.push_back(value);
-    }
+    std::vector<std::vector<double> > getJointVel() const   {return jointVel;}
+    void addJointVel(const std::vector<double> &value)      {jointVel.push_back(value);}
 
-    std::vector<std::vector<double> > getToolPose() const {
-        return toolPose;
-    }
-    void addToolPose(const std::vector<double> &value){
-        toolPose.push_back(value);
-    }
+    std::vector<std::vector<double> > getToolPose() const   {return toolPose;}
+    void addToolPose(const std::vector<double> &value)      {toolPose.push_back(value);}
 
-    std::vector<std::vector<double> > getToolVel() const {
-        return toolVel;
-    }
-    void addToolVel(const std::vector<double> &value){
-        toolVel.push_back(value);
-    }
+    std::vector<std::vector<double> > getToolVel() const    {return toolVel;}
+    void addToolVel(const std::vector<double> &value)       {toolVel.push_back(value);}
 
 private:
     std::vector<std::vector<double>> jointPoses, jointVel, toolPose, toolVel;
